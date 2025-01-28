96ff42f5447ef0baba690995b1b38488d4303dc7
Peter Lucido <127632779+PeterLucido@users.noreply.github.com>
2025-01-28 12:04:27 -0600

    make adjustment

--- app/components/callbutton.tsx
+++ app/components/callbutton.tsx
@@ -5,7 +5,7 @@ interface ButtonComponentProps {
     buttonLink?: string; // Optional in case no link is provided
     buttonText: string;
     className?: string; // Allow custom styling via className
-    Link?: any;
+    Link?: unknown;
 }
 
 const ButtonComponent = ({ onClick, buttonLink, buttonText, className }: ButtonComponentProps) => {
